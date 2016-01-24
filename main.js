5c48d7f8b8220f726df4767bff333552b605e25f
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-01-24 23:27:00 +0600

    planet parameters, fix force computation

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ var App = {
 	height: 150,
 	anim: true,
 	timer: null,
+
+	// preview
+	pre_pos: null, 
+	pre_vel: null, 
+	draw_path: false,
+}
+
+function getNewPlanet() {
+	var m = parseFloat(document.getElementById("input_mass").value);
+	if(m == m && m > 1.0) {
+		return Planet.create(
+			m, App.pre_pos, App.pre_vel, 
+			document.getElementById("input_color").value
+			);
+	}
+	return null;
 }
 
 function pageToWorld(v) {
@@ -17,7 +33,7 @@ function worldToPage(v) {
 function drawPlanet(p) {
 	var ctx = App.canvas.getContext("2d");
 	
-	ctx.fillStyle = "rgb(255,255,255)";
+	ctx.fillStyle = p.color;
 	ctx.beginPath();
 	var ppos = worldToPage(p.pos);
 	ctx.arc(ppos.x, ppos.y, p.rad, 0, 2*Math.PI);
@@ -30,8 +46,22 @@ function render(ticks) {
 	var ctx = App.canvas.getContext("2d");
 	ctx.fillStyle = "rgba(0,0,0,0.05)";
 	ctx.fillRect(0,0,App.width,App.height);
+	//ctx.clearRect(0,0,App.width,App.height);
 
-	for(i = 0; i < Planets.length; ++i) {
+	if(App.draw_path) {
+		var p = getNewPlanet();
+		if(p != null) {
+			drawPlanet(p);
+			/*
+			for(var i = 0; i < 10; ++i) {
+				p = solveSingle(p,0.01,10);
+				drawPlanet(p);
+			}
+			*/
+		}
+	}
+
+	for(var i = 0; i < Planets.length; ++i) {
 		drawPlanet(Planets[i]);
 	}
 
@@ -55,7 +85,7 @@ function resume() {
 	App.timer = setInterval(function() {
 		solve(0.01);
 		removeExoplanets(2*Math.sqrt(App.width*App.width + App.height*App.height));
-		//mergePlanets();
+		mergePlanets();
 	}, 10);
 
 	var bp = $("#button_pause");
@@ -72,30 +102,35 @@ function resize() {
 	App.canvas.height = App.height;
 
 	$("#button_pause").offset({left: 8, top: 8});
-	$("#panel_main").offset({left: App.width - $(panel_main).width() - 8, top: 8});
+	$("#panel_main").offset({left: App.width - $(panel_main).outerWidth() - 8, top: 8});
 
 	console.log("[info] resize {" + App.width + ", " + App.height + "}");
 }
 
-var mouse_down, mouse_up, draw_arrow = false;
 function ready() {
 	console.log("[info] ready");
 
 	App.canvas = document.getElementById("canvas_main");
 	$("#canvas_main")
 	.mousedown(function(e) {
-		mouse_down = Vec2.create(e.pageX, e.pageY);
-		draw_arrow = true;
+		App.pre_pos = pageToWorld(Vec2.create(e.pageX, e.pageY));
+		App.draw_path = true;
+		e.preventDefault();
 	})
 	.mouseup(function(e) {
-		mouse_up = pageToWorld(Vec2.create(e.pageX, e.pageY));
-		mouse_down = pageToWorld(mouse_down);
-		draw_arrow = false;
-		var p = Planet.create(125, mouse_down, Vec2.sub(mouse_up, mouse_down));
-		Planets[Planets.length] = p;
+		var end = pageToWorld(Vec2.create(e.pageX, e.pageY));
+		App.pre_vel = Vec2.sub(end, App.pre_pos);
+		var p = getNewPlanet();
+		if(p != null)
+			Planets[Planets.length] = p;
+		App.draw_path = false;
+		App.pre_pos = null;
 	})
 	.mousemove(function(e) {
-		mouse_up = Vec2.create(e.pageX, e.pageY);
+		if(App.pre_pos != null) {
+			var end = pageToWorld(Vec2.create(e.pageX, e.pageY));
+			App.pre_vel = Vec2.sub(end, App.pre_pos);
+		}
 	});
 
 	$("#button_momentum").click(function() {
--- physics.js
+++ physics.js
@@ -1,12 +1,13 @@
 var Planet = {};
 
 function getRadiusByMass(mass) {
-	return 2*Math.pow(mass, 1.0/3.0);
+	return Math.pow(mass, 1.0/3.0);
 }
 
-Planet.create = function(mass, pos, vel) {
+Planet.create = function(mass, pos, vel, color) {
 	pos = (pos == undefined) ? Vec2.create() : Vec2.clone(pos);
 	vel = (vel == undefined) ? Vec2.create() : Vec2.clone(vel);
+	color = (color == undefined) ? "#FFFFFF" : color;
 	return {
 		pos: pos, 
 		vel: vel, 
@@ -17,47 +18,63 @@ Planet.create = function(mass, pos, vel) {
 		vel_eff: Vec2.clone(vel),
 		pos_deriv: [],
 		vel_deriv: [],
+
+		color: color,
 	};
 };
 
-var g = 4e8;
-var el = 4e5;
+var g = 4e2;
+var el = 1e5;
 
 var Planets = [
 	Planet.create(8000, Vec2.create(0,0), Vec2.create(0,-1)),
-	Planet.create(125, Vec2.create(-300, 0), Vec2.create(0,64)),
+	Planet.create(125, Vec2.create(-300, 0), Vec2.create(0,64), "#00AA00"),
 ];
 
+function getGravityForce(m0, m1, l) {
+	return -g*m0*m1/(l*l);
+}
+
+function getElasticForce(r0, r1, l) {
+	return el*((r0 + r1) - l);
+}
+
+function getForce(a, b) {
+	var d = Vec2.sub(b.pos_eff, a.pos_eff);
+	var l = Math.sqrt(Vec2.dot(d,d));
+	d = Vec2.div(d, l);
+	var f = Vec2.create();
+
+	var ml = a.rad + b.rad;
+	if(l < ml) { 
+		f = Vec2.mul(d, getGravityForce(a.mass, b.mass, ml));
+		f = Vec2.mul(d, getElasticForce(a.rad, b.rad, l));
+	} else {
+		f = Vec2.mul(d, getGravityForce(a.mass, b.mass, l));
+	}
+
+	return f;
+}
+
 function computeDeriv(k) {
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.vel_deriv[k] = Vec2.create();
 	}
 
-	for(i = 0; i < Planets.length; ++i) {
-		for(j = 0; j < i; ++j) {
+	for(var i = 0; i < Planets.length; ++i) {
+		for(var j = 0; j < i; ++j) {
 			var a = Planets[i];
 			var b = Planets[j];
 
-			var d = Vec2.sub(b.pos_eff, a.pos_eff);
-			var l = Math.sqrt(Vec2.dot(d,d));
-			var f = Vec2.create();
+			var f = getForce(a, b);
 
-			var ml = a.rad + b.rad;
-			if(l < ml) { 
-				var dl = ml - l;
-				f = Vec2.add(f, Vec2.mul(Vec2.div(d, ml*ml*l), g)); // Gravity
-				f = Vec2.sub(f, Vec2.mul(d, el*dl/l)); // Elasticity
-			} else {
-				f = Vec2.add(f, Vec2.mul(Vec2.div(d, l*l*l), g)); // Gravity
-			}
-
-			a.vel_deriv[k] = Vec2.add(a.vel_deriv[k], Vec2.div(f, a.mass));
-			b.vel_deriv[k] = Vec2.sub(b.vel_deriv[k], Vec2.div(f, b.mass));
+			a.vel_deriv[k] = Vec2.sub(a.vel_deriv[k], Vec2.div(f, a.mass));
+			b.vel_deriv[k] = Vec2.add(b.vel_deriv[k], Vec2.div(f, b.mass));
 		}
 	}
 
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.pos_deriv[k] = Vec2.clone(p.vel_eff);
 	}
@@ -65,7 +82,7 @@ function computeDeriv(k) {
 
 /* Solver uses the Runge-Kutta method */
 function solve(dt) {
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.pos_eff = Vec2.clone(p.pos);
 		p.vel_eff = Vec2.clone(p.vel);
@@ -73,7 +90,7 @@ function solve(dt) {
 
 	computeDeriv(0);
 
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.pos_eff = Vec2.add(p.pos, Vec2.mul(p.pos_deriv[0], 0.5*dt));
 		p.vel_eff = Vec2.add(p.vel, Vec2.mul(p.vel_deriv[0], 0.5*dt));
@@ -81,7 +98,7 @@ function solve(dt) {
 
 	computeDeriv(1);
 
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.pos_eff = Vec2.add(p.pos, Vec2.mul(p.pos_deriv[1], 0.5*dt));
 		p.vel_eff = Vec2.add(p.vel, Vec2.mul(p.vel_deriv[1], 0.5*dt));
@@ -89,7 +106,7 @@ function solve(dt) {
 
 	computeDeriv(2);
 
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.pos_eff = Vec2.add(p.pos, Vec2.mul(p.pos_deriv[2], dt));
 		p.vel_eff = Vec2.add(p.vel, Vec2.mul(p.vel_deriv[2], dt));
@@ -97,7 +114,7 @@ function solve(dt) {
 
 	computeDeriv(3);
 
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		p.pos = Vec2.add(p.pos, Vec2.mul(
 			Vec2.add(Vec2.add(p.pos_deriv[0], p.pos_deriv[3]), Vec2.mul(Vec2.add(p.pos_deriv[1], p.pos_deriv[2]), 2.0)), 
@@ -108,8 +125,28 @@ function solve(dt) {
 	}
 }
 
+function solveSingle(planet, dt, steps) {
+	var p = planet;
+
+	p.pos_eff = Vec2.clone(p.pos);
+	p.vel_eff = Vec2.clone(p.vel);
+
+	for(var s = 0; s < steps; ++s) {
+		for(var i = 0; i < Planets.length; ++i) {
+			var f = getForce(Planets[i], p);
+			p.vel_eff = Vec2.add(p.vel_eff, Vec2.mul(f, dt/p.mass));
+			p.pos_eff = Vec2.add(p.pos_eff, Vec2.mul(p.vel_eff, 0.5*dt));
+		}
+	}
+
+	p.pos = Vec2.clone(p.pos_eff);
+	p.vel = Vec2.clone(p.vel_eff);
+
+	return p;
+}
+
 function removeExoplanets(radius) {
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		var p = Planets[i];
 		if(Math.sqrt(Vec2.dot(p.pos, p.pos)) > radius) {
 			Planets.splice(i,1);
@@ -119,23 +156,29 @@ function removeExoplanets(radius) {
 	//console.log(Planets.length);
 }
 
+function blendColors(c0, c1, p) {
+    var f=parseInt(c0.slice(1),16),t=parseInt(c1.slice(1),16),R1=f>>16,G1=f>>8&0x00FF,B1=f&0x0000FF,R2=t>>16,G2=t>>8&0x00FF,B2=t&0x0000FF;
+    return "#"+(0x1000000+(Math.round((R2-R1)*p)+R1)*0x10000+(Math.round((G2-G1)*p)+G1)*0x100+(Math.round((B2-B1)*p)+B1)).toString(16).slice(1);
+}
+
 function mergePlanets() {
-	for(i = 0; i < Planets.length; ++i) {
-		for(j = 0; j < i; ++j) {
+	for(var i = 0; i < Planets.length; ++i) {
+		for(var j = 0; j < i; ++j) {
 			var a = Planets[i];
 			var b = Planets[j];
 			
 			var d = Vec2.sub(b.pos, a.pos);
 			var l = Math.sqrt(Vec2.dot(d,d));
-			if(l < 2.0*(a.rad + b.rad)) {
+			if(l < 0.5*(a.rad + b.rad)) {
 				var mass = a.mass + b.mass;
-				b.pos = Vec2.div(Vec2.sum(Vec2.mul(a.pos, a.mass), Vec2.mul(b.pos, b.mass)), mass);
-				b.vel = Vec2.div(Vec2.sum(Vec2.mul(a.vel, a.mass), Vec2.mul(b.vel, b.mass)), mass);
+				b.pos = Vec2.div(Vec2.add(Vec2.mul(a.pos, a.mass), Vec2.mul(b.pos, b.mass)), mass);
+				b.vel = Vec2.div(Vec2.add(Vec2.mul(a.vel, a.mass), Vec2.mul(b.vel, b.mass)), mass);
 				b.mass = mass;
 				b.rad = getRadiusByMass(mass);
+				b.color = blendColors(b.color, a.color, a.mass/mass);
 				Planets.splice(i,1);
 				--i;
-				break;
+				return;
 			}
 		}
 	}
@@ -144,12 +187,12 @@ function mergePlanets() {
 function zeroBarycenter() {
 	var position = Vec2.create();
 	var mass = 0.0;
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		position = Vec2.add(position, Vec2.mul(Planets[i].pos, Planets[i].mass));
 		mass += Planets[i].mass;
 	}
 	var center = Vec2.div(position, mass);
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		Planets[i].pos = Vec2.sub(Planets[i].pos, center);
 	}
 }
@@ -157,12 +200,12 @@ function zeroBarycenter() {
 function zeroMomentum() {
 	var momentum = Vec2.create();
 	var mass = 0.0;
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		momentum = Vec2.add(momentum, Vec2.mul(Planets[i].vel, Planets[i].mass));
 		mass += Planets[i].mass;
 	}
 	var velocity = Vec2.div(momentum, mass);
-	for(i = 0; i < Planets.length; ++i) {
+	for(var i = 0; i < Planets.length; ++i) {
 		Planets[i].vel = Vec2.sub(Planets[i].vel, velocity);
 	}
 }
